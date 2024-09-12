fbfa8f5c06a6c7050fee66bb4846e0639843c229
Raghav Katta <Raghavkatta2005@gmail.com>
2024-09-12 23:54:48 +0530

    added navbar

--- src/components/Main.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-export default function Navbar(){
-    return(
-        <div className="container">
-            <h1>Navbar comes here</h1>
-        </div>
-    )
-}
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+export default function Navbar(){
+    return(
+        <nav>
+         
+           <h3 className="logo--text">ReactFacts<br/></h3>
+           <h4 className="nav--text">React Course - Project-1</h4>
+       </nav>
+    )
+}
