372651d0a0b38fb4448996b26e92e440e0ca1e9b
Raghav Katta <Raghavkatta2005@gmail.com>
2024-09-15 23:57:33 +0530

    added main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,15 +1,15 @@
 import React from "react";
-export default function Main(){
-    return(
+export default function Main() {
+    return (
         <main>
-           <h1 className="FactTitle">Fun Facts about React</h1>
-           <ul className="list">
-            <li>Was first released in 2013</li>
-            <li>Was originally created by Jordan Walke</li>
-            <li>Has well over 100K stars on GitHub</li>
-            <li>is maintained by Facebook</li>
-            <li>Powers thousands of enterprise apps including mobile apps</li>
-           </ul>
+            <h1 className="main--Title">Fun Facts about React</h1>
+            <ul className="main--facts">
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100K stars on GitHub</li>
+                <li>is maintained by Facebook</li>
+                <li>Powers thousands of enterprise apps including mobile apps</li>
+            </ul>
         </main>
     )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-export default function Navbar(){
-    return(
+import reactLogo from "../assets/ReactLogo.webp"
+export default function Navbar() {
+    return (
         <nav>
-         
-           <h3 className="logo--text">ReactFacts<br/></h3>
-           <h4 className="nav--text">React Course - Project-1</h4>
-       </nav>
+            <img src={reactLogo} className="nav--logo"></img>
+            <h3 className="logo--text">ReactFacts<br /></h3>
+            <h4 className="nav--text">React Course - Project-1</h4>
+        </nav>
     )
 }
